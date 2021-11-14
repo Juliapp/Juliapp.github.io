2efb44c63675ea2e1af2eae477179d148c6ab7e0
Juliana <aragaopintojuli@gmail.com>
2021-11-14 20:41:03 -0300

    installing svelte full page

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name = "World";
+	// export let name = "World";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>This page is will be up very soon. Stay Tuned!.</p>
+	<!-- <section>
+		<h1>Códigos geniais</h1>
+		<h1>Softwares incíveis.</h1>
+	</section>
+	<section>
+		<h3>Olá, eu sou Juliana Pinto, estudante e</h3>
+		<h3>Desenvolvedora de software Full Stack</h3>
+	</section> -->
 </main>
+
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 100vw;
+		height: 100vh;
+		background-image: url('./assets/bg.png');
+		background-size: 100%;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	section {
+		font-family: Montserrat sans-serif;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
